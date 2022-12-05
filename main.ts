eda1a8c0f62228c37e36745f7a87e37e8da93335
Zwiggy <118271010+Zwiggy69@users.noreply.github.com>
2022-12-05 15:58:14 +0100

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,16 +1,83 @@
 function ventilator_uit () {
-    if (input.buttonIsPressed(Button.B)) {
-        pins.digitalWritePin(DigitalPin.P0, 0)
-    }
+    kitronik_smart_greenhouse.controlHighPowerPin(kitronik_smart_greenhouse.HighPowerPins.pin13, kitronik_smart_greenhouse.onOff(false))
 }
+input.onButtonPressed(Button.A, function () {
+    gwwt += 1
+    temp += gwwt
+    basic.showLeds(`
+        . # # # .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+    basic.showNumber(gwwt)
+})
+input.onButtonPressed(Button.B, function () {
+    gwwt += -1
+    temp += gwwt
+    basic.showLeds(`
+        . # # # .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+    basic.showNumber(gwwt)
+})
 function ventilator_aan () {
-    if (input.buttonIsPressed(Button.A)) {
-        pins.digitalWritePin(DigitalPin.P0, 1)
-    }
+    kitronik_smart_greenhouse.controlHighPowerPin(kitronik_smart_greenhouse.HighPowerPins.pin13, kitronik_smart_greenhouse.onOff(true))
 }
+function verwarming_uit () {
+    kitronik_smart_greenhouse.controlHighPowerPin(kitronik_smart_greenhouse.HighPowerPins.pin14, kitronik_smart_greenhouse.onOff(false))
+}
+function verwarmin_aan () {
+    kitronik_smart_greenhouse.controlHighPowerPin(kitronik_smart_greenhouse.HighPowerPins.pin14, kitronik_smart_greenhouse.onOff(true))
+}
+let temp = 0
+let gwwt = 0
+gwwt = 22
+temp += gwwt
 basic.forever(function () {
-    ventilator_aan()
+    temp = input.temperature()
+    basic.showNumber(temp)
+    basic.pause(5000)
 })
 basic.forever(function () {
-    ventilator_uit()
+    basic.showString("" + (input.temperature()))
+    basic.pause(2000)
+})
+basic.forever(function () {
+    if (input.temperature() > 22) {
+        ventilator_aan()
+    }
+})
+basic.forever(function () {
+    if (input.temperature() < 22) {
+        verwarmin_aan()
+    }
+})
+basic.forever(function () {
+    if (input.temperature() <= 22) {
+        ventilator_uit()
+    }
+})
+basic.forever(function () {
+    if (input.temperature() >= 22) {
+        verwarming_uit()
+    }
 })
